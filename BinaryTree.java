c3fac1ab85a2ab0a0c948250d06ceaf95fa79a0e
ximelh <ximenaloarca@gmail.com>
2022-03-29 11:34:25 -0600

    Implementacion binary trees

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,3 +1,80 @@
-public class BinaryTree {
-    
+public class BinaryTree<E>
+{
+
+    protected E val; // value associated with node
+	protected BinaryTree<E> parent; // parent of node
+	protected BinaryTree<E> left, right; // children of node
+	
+	public BinaryTree()
+	// post: constructor that generates an empty node
+	{
+		val = null;
+		parent = null; left = right = this;
+	}
+	
+	public BinaryTree(E value)
+	// post: returns a tree referencing value and two empty subtrees
+	{
+		val = value;
+		right = left = new BinaryTree<E>();
+		setLeft(left);
+		setRight(right);
+	}
+	
+	public BinaryTree(E value, BinaryTree<E> left, BinaryTree<E> right)
+	// post: returns a tree referencing value and two subtrees
+	{
+		val = value;
+		if (left == null) { left = new BinaryTree<E>(); }
+		setLeft(left);
+		if (right == null) { right = new BinaryTree<E>(); }
+		setRight(right);
+	}
+	
+	public BinaryTree<E> left()
+	// post: returns reference to (possibly empty) left subtree
+	// post: returns reference to (possibly empty) left subtree
+	{
+		return left;
+	}
+	
+	public BinaryTree<E> parent()
+	// post: returns reference to parent node, or null
+	
+	public void setLeft(BinaryTree<E> newLeft)
+	// post: sets left subtree to newLeft
+	// re-parents newLeft if not null
+	{
+		if (isEmpty()) return;
+		if (left != null && left.parent() == this) left.setParent(null);
+		left = newLeft;
+		left.setParent(this);
+	}
+	
+	protected void setParent(BinaryTree<E> newParent)
+	// post: re-parents this node to parent reference, or null
+	{
+		if (!isEmpty()) {
+		parent = newParent;
+		}
+	}
+	
+	public Iterator<E> iterator()
+	// post: returns an in-order iterator of the elements
+	
+	public boolean isLeftChild()
+	// post: returns true if this is a left child of parent
+	
+	public E value()
+	// post: returns value associated with this node
+	{
+		return val;
+	}
+	
+	public void setValue(E value)
+	// post: sets the value associated with this node
+	{
+		val = value;
+	}
+	
 }
---
+++ ComparableAssociation.java
@@ -0,0 +1,63 @@
+
+import java.util.Map;
+public class ComparableAssociation<K extends Comparable<K>,V>
+    extends Association<K,V>
+    implements Comparable<ComparableAssociation<K,V>>
+    , Map.Entry<K,V>
+{
+    /**
+     * Construct an association that can be ordered, from only a key.
+     * The value is set to null.
+     *
+     * @pre key is non-null
+     * @post constructs comparable association with null value
+     * 
+     * @param key The (comparable) key.
+     */
+    public ComparableAssociation(K key)
+    {
+        this(key,null);
+    }
+
+    /**
+     * Construct a key-value association that can be ordered.
+     *
+     * @pre key is non-null
+     * @post constructs association between a comparable key and a value
+     * 
+     * @param key The (comparable) key.
+     * @param value The (possibly comparable) associated value.
+     */
+    public ComparableAssociation(K key, V value)
+    {
+        super(key,value);
+    }
+
+    /**
+     * Determine the order of two comparable associations, based on key.
+     *
+     * @pre other is non-null ComparableAssociation
+     * @post returns integer representing relation between values
+     * 
+     * @param other The other comparable association.
+     * @return Value less-than equal to or greater than zero based on comparison
+     */
+    public int compareTo(ComparableAssociation<K,V> that)
+    {
+        return this.getKey().compareTo(that.getKey());
+    }
+
+    /**
+     * Construct a string representation of the ComparableAssociation.
+     *
+     * @post returns string representation
+     * 
+     * @return The string representing the ComparableAssociation.
+     */
+    public String toString()
+    {
+        StringBuffer s = new StringBuffer();
+        s.append("<ComparableAssociation: "+getKey()+"="+getValue()+">");
+        return s.toString();
+    }
+}
